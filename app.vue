1a66adc7d131a566269a459d9b9ed7094054bb4e
phihungtran <phihung99nd@gmail.com>
2024-04-24 19:13:13 +0700

    Add seoMeta

--- app.vue
+++ app.vue
@@ -3,3 +3,14 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Teri Portfolio v2',
+  ogTitle: 'Teri Portfolio v2',
+  description: 'My portfolio version 2 using NuxtJS',
+  ogDescription: 'My portfolio version 2 using NuxtJS',
+  ogImage: '/images/ogImage.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
